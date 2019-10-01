cb514067902ba4219494f143e2a7aa71a61c2d8e
Vaibhav Yadav <vaibhavy131@gmail.com>
2019-10-01 16:34:00 +0530

    created capitalize algorithm for contribution

---
+++ capitalize.js
@@ -0,0 +1,25 @@
+/*
+Create a function that takes an array of names and returns an array with the first letter capitalized.
+
+Examples
+capitalize(["mavis", "senaida", "letty"]) ➞ ["Mavis", "Senaida", "Letty"]
+
+capitalize(["samuel", "MABELLE", "letitia", "meridith"]) ➞ ["Samuel", "Mabelle", "Letitia", "Meridith"]
+
+capitalize(["Slyvia", "Kristal", "Sharilyn", "Calista"]) ➞ ["Slyvia", "Kristal", "Sharilyn", "Calista"]
+Notes
+Don't change the order of the original array.
+Notice in the second example above, "MABELLE" is returned as "Mabelle".
+
+After writing the solution run ```npm run capitalize-test``` to test your solution
+
+source: edabit.com
+*/
+
+function capitalize(arr) {
+  // Do not change anything above this line
+  // Write your code here
+}
+// Do not change anything below this line
+
+module.exports = capitalize;
